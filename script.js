2327e66fa7799bed150bcca0ac33c185581da1f2
Vorezii <41553655+Vorezii@users.noreply.github.com>
2021-06-08 19:42:17 -0700

    Open Overaly based on URL

--- script.js
+++ script.js
@@ -1,25 +1,31 @@
 const overlay = document.querySelector('.overlay');
-const images = document.querySelectorAll('.image img');
 const popImg = document.querySelector('.popup-content img');
 const caption = document.querySelector('.caption');
 const closeBTN = document.querySelector('.close-button');
+const popUpsImages = document.querySelectorAll('.image img');
 
-images.forEach(images => {
-    images.addEventListener('click', () => {
+const currentLocation = location.href;
+const popUps = document.querySelectorAll('.image');
+
+//this opens overlay and chooses images based on url
+for(let i = 0; i < popUps.length; i++){
+    if(popUps[i].href == currentLocation){
         overlay.classList.add("open");
-        const imgSrc = images.getAttribute('src');
+        const imgSrc = popUpsImages[i].getAttribute('src');
         popImg.src = imgSrc;
-        const altText = images.title;
+        const altText = popUpsImages[i].title;
         caption.textContent = altText;
-    })
-})
+    }
+}
 
+//closes overlay when clicked on translucent area
 overlay.addEventListener('click', (e)=> {
     if(e.target.classList.contains('overlay')){
         overlay.classList.remove('open');
     }
 })
 
+//closes overlay when clostBTN is clicked
 closeBTN.addEventListener('click', (e)=> {
     overlay.classList.remove('open');
 })
